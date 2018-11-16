4b6385aaa0c0d812708449ba1fff8c417bebf708
Richard Kim <kimr@mit.edu>
2018-11-16 15:20:51 -0500

    first commit

---
+++ config.py
@@ -0,0 +1,78 @@
+COURSE_NAME = '2019-CS109B'
+
+AUTHOR = ''
+
+SITEURL = ''
+
+GITHUB = ''
+
+COLOR = '#A51C30'
+
+MENUITEMS = []
+
+PATH = 'content'
+
+OUTPUT_PATH = 'docs'
+
+TIMEZONE = 'EST'
+
+DEFAULT_LANG = 'en'
+
+FEED_ALL_ATOM = None
+
+CATEGORY_FEED_ATOM = None
+
+TRANSLATION_FEED_ATOM = None
+
+AUTHOR_FEED_ATOM = None
+
+AUTHOR_FEED_RSS = None
+
+DEFAULT_PAGINATION = 10
+
+DEFAULT_CATEGORY = 'pages'
+
+AUTHORS_SAVE_AS = ''
+
+CATEGORIES_SAVE_AS = ''
+
+ARCHIVES_SAVE_AS = ''
+
+ARTICLE_SAVE_AS = '{category}/{slug}/index.html'
+
+ARTICLE_URL = '{category}/{slug}/'
+
+AUTHOR_URL = ''
+
+AUTHOR_SAVE_AS = ''
+
+INDEX_SAVE_AS = 'pages/materials.html'
+
+THEME_STATIC_DIR = 'style'
+
+DELETE_OUTPUT_DIRECTORY = True
+
+MARKUP = ['md', 'ipynb']
+
+PLUGIN_PATHS = ['plugins']
+
+PLUGINS = ['ipynb.markup', 'tipue_search']
+
+IGNORE_FILES = ['.ipynb_checkpoints']
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+STATIC_PATHS = ['lectures', 'labs', 'homeworks', 'sections', 'wiki', 'images', 'projects']
+
+DIRECT_TEMPLATES = ['index', 'category', 'tags', 'search']
+
+import re
+
+JINJA_FILTERS = {
+    'original_content': lambda x: re.search(r"content/.*", x).group(0)
+}
+
+USE_FOLDER_AS_CATEGORY = False
+
+IGNORE_FILES = ['README.md']
